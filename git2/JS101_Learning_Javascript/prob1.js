924a154b7e282ff780fab95e1849189d6383c336
MrPandeyAN97 <124018337+MrPandeyAN97@users.noreply.github.com>
2023-02-05 20:33:25 +0530

    Add files via upload

---
+++ git2/JS101_Learning_Javascript/prob1.js
@@ -0,0 +1,30 @@
+let name1="masai";
+let name2="kasai";
+
+if(name1==name2){
+  console.log("same name");
+}
+else{
+  console.log("not same");
+}
+
+if(name1!=name2){
+  console.log("same name");
+}
+else{
+  console.log("not same");
+}
+
+if(name1!==name2){
+  console.log("same name");
+}
+else{
+  console.log("not same");
+}
+
+if(name1==name2){
+  console.log("same name");
+}
+else{
+  console.log("not same");
+}
---
+++ git2/JS101_Learning_Javascript/prob2.js
@@ -0,0 +1,9 @@
+let item_price=1000;
+let money_you_have=500;
+
+if(money_you_have>=item_price){
+  console.log("i bought the item");
+}
+else{
+  console.log("dont have money");
+}
